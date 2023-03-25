1d2dcacc3e3c2e14652a809f586cd08f4ce420e3
Pais Jose Murama <paizinhomurama@gmail.com>
2023-03-25 14:32:45 +0200

    fixing index.js

--- index.js
+++ index.js
@@ -3,8 +3,14 @@
 
 */
 const express = require('express')    //requering the express framework!
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const admin = require ('firebase-admin');
+let inspect = require('util').inspect;
+let Busboy = require('busboy');
+let path = require('path')
+let os = require('os')
+let fs = require('fs')
+let UUID = require('uuid-v4')
+
 
 /*
  config-express
@@ -12,19 +18,22 @@ const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestor
   const app = express()  //Criando uma app para que possamos fazer algumas coisas com o express
 
 
+  /*config-firebase*/
 
+  const serviceAccount = require('./serviceAccountKey.json');
 
-     const serviceAccount = require('./serviceAccountKey.json');
+     admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        storageBucket: "quasargram-7b6a9.appspot.com"
 
-      initializeApp({
-            credential: cert(serviceAccount)
-          });
+      });
 
-      const db = getFirestore();
+          const db = admin.firestore()
+          let bucket = admin.storage().bucket()
 
-
-
-/***Endpoints - posts**/
+/***
+ * endpoints - posts
+ * **/
 
 app.get('/posts', (request, response) => {  //Configuração de endpoints para obter solicitações
   response.set('Access-Control-Allow-Origin', '*')
@@ -46,13 +55,66 @@ app.get('/posts', (request, response) => {  //Configuração de endpoints para o
 
 
 /***Endpoints - createPosts**/
-
-app.get('/createPost', (request, response) => {  //Configuração de endpoints para obter solicitações
+app.post('/createPost', (request, response) => {
   response.set('Access-Control-Allow-Origin', '*')
 
-   response.send('createPost');
 
 
+   let uuid = UUID()
+   var busboy = new Busboy({ headers: request.headers });
+   let fields = {}
+   let fileData = {}
+
+  busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+    console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+
+    // /tmp/4564564-234234.png
+    let filepath = path.join(os.tmpdir(),filename)
+    file.pipe(fs.createWriteStream(filepath))
+    fileData = {filepath,mimetype}
+
+  });
+
+  busboy.on('field', function (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+    fields[fieldname] = val
+  });
+
+  busboy.on('finish', function () {
+   bucket.upload(
+    fileData.filepath,
+    {
+      uploadType:'media',
+      metadata:{
+        metadata:{
+          contentType:fileData.mimetype,
+          firebaseStorageDownloadTokens:uuid
+        }
+      }
+    },
+    (err, uploadedFile) =>{
+       if(!err)
+       {
+        createDocument(uploadedFile)
+       }
+    }
+   )
+
+   function createDocument(uploadedFile) {
+
+    db.collection('posts').doc(fields.id).set({
+      id:fields.id,
+      caption:fields.caption,
+      location: fields.location,
+      date: parseInt(fields.date),
+      imageUrl:`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${uploadedFile.name}?alt=media&token=${uuid}`
+    }).then(()=>{
+      response.send('Post added:' +fields.id)
+    })
+
+   }
+    // response.writeHead(303, { Connection: 'close', Location: '/' });
+  });
+  request.pipe(busboy);
 })
 
 
